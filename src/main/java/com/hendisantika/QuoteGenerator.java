b6f4ba8218861fd0b2ba194e3cbadff0cf274e56
hendisantika <hendisantika@yahoo.co.id>
2023-12-21 14:12:30 +0700

    Add generateQuote in Quote Generator 🫸🌀✏️📗🐧🐳

--- src/main/java/com/hendisantika/QuoteGenerator.java
+++ src/main/java/com/hendisantika/QuoteGenerator.java
@@ -4,7 +4,11 @@ import io.smallrye.mutiny.Multi;
 import jakarta.enterprise.context.ApplicationScoped;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.eclipse.microprofile.reactive.messaging.Outgoing;
+import yahoofinance.Stock;
+import yahoofinance.YahooFinance;
 
+import java.io.IOException;
+import java.math.BigDecimal;
 import java.time.Duration;
 import java.util.List;
 
@@ -31,4 +35,23 @@ public class QuoteGenerator {
         return Multi.createFrom().ticks().every(Duration.ofSeconds(1)).map(n -> generateQuote());
     }
 
+    private Quote generateQuote() {
+        Stock stock = null;
+        String ticker = stocks.get(getCounter());
+        try {
+            stock = YahooFinance.get(ticker);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        BigDecimal price = stock.getQuote().getPrice();
+        BigDecimal priceChange = stock.getQuote().getChange();
+
+        Quote q = new Quote();
+        q.setCompany(ticker);
+        q.setValue(price.doubleValue());
+        q.setChange(priceChange.doubleValue());
+
+        return q;
+    }
 }
